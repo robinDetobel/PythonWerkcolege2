2243811ff4013077fb7824fa33a738e9653f77bc
robinDetobel <robin.detobel@student.ehb.be>
2020-09-23 15:34:48 +0200

    testen op try except met oefening 3

---
+++ TryExceptTesting.py
@@ -0,0 +1,27 @@
+offertes = []
+tot = 0
+invoer = 0
+
+try:
+    invoer = float(input("Geef de prijs van de offerte in: "))
+except ValueError:
+    print("Oops!  Dit was geen getal, probeer het opnieuw.")
+except ZeroDivisionError:
+    print("Oops!  Dit was geen getal, probeer het opnieuw.")
+
+while invoer != -1:
+    if invoer < 0:
+        print("Ongeldig getal ingegeven")
+    else:
+        offertes.append(invoer)
+    try:
+        invoer = float(input("Geef de prijs van de offerte in: "))
+    except ValueError:
+        print("Oops!  Dit was geen getal, probeer het opnieuw.")
+    except ZeroDivisionError:
+        print("Oops!  Dit was geen getal, probeer het opnieuw.")
+
+
+for prijs in offertes:
+    tot += prijs
+
